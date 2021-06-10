f7e04cfffe200758fc740429eab5603d2f9c4ea9
John Lago <750845+Lagoja@users.noreply.github.com>
2021-06-10 08:13:39 -0700

    remove app from cron job

--- cron-job/main.py
+++ cron-job/main.py
@@ -4,14 +4,13 @@ from flask import Flask
 import requests
 import time
 
-app = Flask(__name__)
 
 headers = {"content-type": "application/json", "Accept-Charset": "UTF-8"}
 url = environ.get("SLACK_URL")
 
 
 @cron.repeat(cron.every(10).minutes)
-def cron_job_ten():
+def cron_job():
     payload = '{"text":"I\'m a cronjob that runs every 10 minutes"}'
     time.sleep(20)
     requests.post(url, data=payload, headers=headers)
