fca542e7351ea0e8ee455ddceb73116da879f065
John Lago <750845+Lagoja@users.noreply.github.com>
2021-06-10 09:33:14 -0700

    Remove unnecessary code

--- cron-job/main.py
+++ cron-job/main.py
@@ -1,15 +1,10 @@
-from jetpack import cron
 from os import environ
-from flask import Flask
 import requests
 import time
-
 
 headers = {"content-type": "application/json", "Accept-Charset": "UTF-8"}
 url = environ.get("SLACK_URL")
 
-
-@cron.repeat(cron.every(10).minutes)
 def cron_job():
     payload = '{"text":"I\'m a cronjob that runs every 10 minutes"}'
     time.sleep(20)
